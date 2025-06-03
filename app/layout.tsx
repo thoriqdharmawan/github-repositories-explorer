dd7beac8898d7ccce932fa1a12c84d41a6b81abe
thoriqdharmawan <thoriqd21@gmail.com>
2025-06-03 10:46:14 +0700

    feat: refactor ThemeProvider component and update import path in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
+import { ThemeProvider } from "@/components/ThemeProvider";
 import { QueryProvider } from "@/providers/QueryProvider";
 
 const geistSans = Geist({
