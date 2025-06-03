59b16ae3003a6bbc79e6c039119d42b7ab69b44d
thoriqdharmawan <thoriqd21@gmail.com>
2025-06-03 16:42:48 +0700

    feat: restructure RepoItem component for improved layout and display of repository details

--- features/list-repos/RepoItem.tsx
+++ features/list-repos/RepoItem.tsx
@@ -1,4 +1,5 @@
 import { Repo } from "@/types/repos";
+import { BookMarked, Star } from "lucide-react";
 import { FC } from "react";
 
 interface RepoItem {
@@ -8,22 +9,27 @@ interface RepoItem {
 const RepoItem: FC<RepoItem> = ({ repo }) => {
   return (
     <div className="rounded-lg border p-4 shadow-sm transition-shadow hover:shadow-md">
-      <h3 className="mb-2 text-lg font-semibold text-gray-900">{repo.name}</h3>
+      <div className="mb-2 flex items-center justify-between">
+        <div className="flex items-center justify-center gap-2">
+          <BookMarked />
+          <a href={repo.html_url} target="_blank" rel="noopener noreferrer">
+            <h3 className="text-lg font-semibold text-gray-900 hover:underline">
+              {repo.name || "Unknown Repository"}
+            </h3>
+          </a>
+        </div>
+
+        <div className="flex items-center gap-2 text-gray-500">
+          <p>{repo.stargazers_count}</p>
+          <Star size={16} />
+        </div>
+      </div>
 
       <p className="text-sm text-gray-600">
         {repo.description || (
           <span className="italic">No description available.</span>
         )}
       </p>
-
-      <a
-        href={repo.html_url}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-blue-600 hover:underline"
-      >
-        {repo.html_url}
-      </a>
     </div>
   );
 };
