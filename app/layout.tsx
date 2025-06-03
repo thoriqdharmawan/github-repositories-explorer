994c684a44eba9327b0d681b71f19dd5eef4f480
thoriqdharmawan <thoriqd21@gmail.com>
2025-06-03 10:46:30 +0700

    feat: move ThemeProvider to providers directory and update import path in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/ThemeProvider";
+import { ThemeProvider } from "@/providers/ThemeProvider";
 import { QueryProvider } from "@/providers/QueryProvider";
 
 const geistSans = Geist({
