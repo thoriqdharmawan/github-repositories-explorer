646dea3a55673965d75939368b50f978fa069319
thoriqdharmawan <thoriqd21@gmail.com>
2025-06-03 19:29:56 +0700

    fix: remove unnecessary height from main container in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import ListUsers from "@/features/list-users";
 
 export default function Home() {
   return (
-    <main className="container mx-auto py-6 h-screen">
+    <main className="container mx-auto py-6">
       <ListUsers />
     </main>
   );
