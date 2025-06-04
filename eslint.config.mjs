694034c891031f3e76b249aad7b48ae0edcabb19
thoriqdharmawan <thoriqd21@gmail.com>
2025-06-04 16:22:19 +0700

    feat: add custom ESLint rules for test files to improve code quality

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,16 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: [
+      "**/__test__/**/*.{ts,tsx}",
+      "**/*.test.{ts,tsx}",
+      "**/*.spec.{ts,tsx}",
+    ],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
