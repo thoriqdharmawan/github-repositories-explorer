32bc8920012e25847ee165bed4fb5cf4a1789b37
thoriqdharmawan <thoriqd21@gmail.com>
2025-06-04 10:10:27 +0700

    fix: remove unused import of getAuthToken from AuthProvider

--- providers/AuthProvider.tsx
+++ providers/AuthProvider.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { createContext, useContext, useEffect, useState } from "react";
-import { getAuthToken, getAuthUser, isAuthenticated } from "@/utils";
+import { getAuthUser, isAuthenticated } from "@/utils";
 
 interface AuthContextType {
   authenticated: boolean;
